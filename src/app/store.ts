d231acb968b1953539c04efdcecd40b67a0b9d66
Neil Gebhard <neilgebhard@gmail.com>
2022-03-19 01:09:00 +1000

    :sparkles: Add todo to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import todo from "../features/todo/todoSlice";
+import counter from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    todo,
+    counter,
   },
 });
 
